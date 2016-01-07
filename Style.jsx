6a9a78a1009a0c9de3e1fb1949bae4ada6234917
chenckang <chenckang@gmail.com>
2016-01-08 00:19:34 +0800

    fix bugs

--- Style.jsx
+++ Style.jsx
@@ -4,6 +4,15 @@ var Style = React.createClass({
   render: function () {
     var style = this.props.style()
 
+    // 如果当前是前端渲染，则上面的style()调用已经完胜style的标签追加
+    if (typeof document === 'object') {
+      var styleDOM = document.getElementById(style.id);
+      if (styleDOM) {
+        return null
+      }
+    }
+
+    // 如果是后端渲染，则返回占位标签，交由express的拦截器处理
     return (
       <style id={style.id} data-node="node-stylus">{style.css}</style>
     )
